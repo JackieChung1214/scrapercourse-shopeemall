fff45e949cc7c79b74184e6fdbde6f70977c14be
Jackie Chung <jackiechung1214@gmail.com>
2022-07-02 15:59:16 +0800

    5.7 批次爬取多分頁的網頁資料

--- scraper.py
+++ scraper.py
@@ -6,34 +6,38 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 driver=webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get('https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
-time.sleep(5)
+for page in range(1,3):
 
-ActionChains(driver).move_by_offset(100,100).click().perform()
 
-cards=driver.find_elements(By.CSS_SELECTOR,"div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
+    driver.get(f'https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925/popular?pageNumber={page}')
+    time.sleep(5)
 
-items=[]
-for card in cards:
-    title=card.find_element(By.CSS_SELECTOR,"div[class='ie3A+n bM+7UW Cve6sh']").text
-    price=card.find_element(By.CSS_SELECTOR,"span[class='ZEgDH9']").text
-    link=card.find_element(By.TAG_NAME,"a").get_attribute('href')
-    items.append((title,price,link))
+    ActionChains(driver).move_by_offset(100,100).click().perform()
 
-#print(items)
+    cards=driver.find_elements(By.CSS_SELECTOR,"div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
 
-result=[]
-for item in items:
-    driver.get(item[2])
+    items=[]
+    for card in cards:
+        title=card.find_element(By.CSS_SELECTOR,"div[class='ie3A+n bM+7UW Cve6sh']").text
+        price=card.find_element(By.CSS_SELECTOR,"span[class='ZEgDH9']").text
+        link=card.find_element(By.TAG_NAME,"a").get_attribute('href')
+        items.append((title,price,link))
 
-    #設定滑鼠滾動次數
-    for i in range(5):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        time.sleep(3)
+    #print(items)
 
-    comments=driver.find_elements(By.CSS_SELECTOR,"div[class='Em3Qhp']")
-    for comment in comments:
-        result.append((item[0],item[1],comment.text))
-    break
+    result=[]
+    for item in items:
+        driver.get(item[2])
 
-print(result)
+        #設定滑鼠滾動次數
+        for i in range(5):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            time.sleep(3)
+
+        comments=driver.find_elements(By.CSS_SELECTOR,"div[class='Em3Qhp']")
+        for comment in comments:
+            result.append((item[0],item[1],comment.text))
+        break
+
+    print(f"第{page}頁")
+    print(result)
